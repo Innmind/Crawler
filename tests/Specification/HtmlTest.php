32269bce2894fa13720a140fb14ed28812ce97a7
Baptiste Langlade <baptouuuu@gmail.com>
2016-10-30 12:48:53 +0100

    add specification to know if a media type can be an html document

---
+++ tests/Specification/HtmlTest.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Crawler\Specification;
+
+use Innmind\Crawler\Specification\html;
+use Innmind\Filesystem\MediaType\MediaType;
+
+class HtmlTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider cases
+     */
+    public function testIsSatisfiedBy(bool $expected, string $type)
+    {
+        $spec = new Html;
+
+        $this->assertSame(
+            $expected,
+            $spec->isSatisfiedBy(MediaType::fromString($type))
+        );
+    }
+
+    public function cases(): array
+    {
+        return [
+            [true, 'text/html'],
+            [true, 'text/xml'],
+            [true, 'application/xhtml+xml'],
+            [true, 'application/xml'],
+            [false, 'application/vnd.sealedmedia.softseal-html'],
+            [false, 'application/vnd.pwg-xhtml-print+xml'],
+            [false, 'application/vnd.oipf.dae.xhtml+xml'],
+            [false, 'application/vnd.ms-htmlhelp'],
+            [false, 'application/vnd.dtg.local-html'],
+        ];
+    }
+}
