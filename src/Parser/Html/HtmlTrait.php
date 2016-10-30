813550fde677fbd4cc8bfd393bdf787dc15a1ab2
Baptiste Langlade <baptouuuu@gmail.com>
2016-10-30 14:40:26 +0100

    add title parser

---
+++ src/Parser/Html/HtmlTrait.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Crawler\Parser\Html;
+
+use Innmind\Crawler\{
+    Parser\Http\ContentTypeParser,
+    Specification\Html
+};
+use Innmind\Immutable\MapInterface;
+
+trait htmlTrait
+{
+    private function isHtml(MapInterface $attributes): bool
+    {
+        return $attributes->contains(ContentTypeParser::key()) &&
+            (new Html)->isSatisfiedBy(
+                $attributes->get(ContentTypeParser::key())->content()
+            );
+    }
+}
