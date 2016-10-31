e7fb1ba1e3b3f7b21972115ebd13f2e8d8d2ba63
Baptiste Langlade <baptouuuu@gmail.com>
2016-10-31 15:00:28 +0100

    add conditional parser

---
+++ src/Parser/ConditionalParser.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Crawler\Parser;
+
+use Innmind\Crawler\ParserInterface;
+use Innmind\Http\Message\{
+    RequestInterface,
+    ResponseInterface
+};
+use Innmind\Immutable\MapInterface;
+
+/**
+ * Iterate over all parsers until one modifies the attributes
+ */
+final class ConditionalParser implements ParserInterface
+{
+    private $parsers;
+    private $length;
+
+    public function __construct(ParserInterface ...$parsers)
+    {
+        $this->parsers = $parsers;
+        $this->length = count($parsers);
+    }
+
+    public function parse(
+        RequestInterface $request,
+        ResponseInterface $response,
+        MapInterface $attributes
+    ): MapInterface {
+        $original = $attributes;
+
+        for ($i = 0; $i < $this->length; $i++) {
+            $attributes = $this->parsers[$i]->parse(
+                $request,
+                $response,
+                $attributes
+            );
+
+            if ($attributes !== $original) {
+                break;
+            }
+        }
+
+        return $attributes;
+    }
+
+    public static function key(): string
+    {
+        return 'conditional';
+    }
+}
