ffc1b539a659a0f820d35c2d402a25a6e9f7fea0
Baptiste Langlade <baptouuuu@gmail.com>
2016-10-31 14:24:52 +0100

    add html charset parser

---
+++ src/Parser/Html/CharsetParser.php
@@ -0,0 +1,97 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Crawler\Parser\Html;
+
+use Innmind\Crawler\{
+    ParserInterface,
+    HttpResource\Attribute
+};
+use Innmind\Xml\{
+    ReaderInterface,
+    NodeInterface,
+    ElementInterface
+};
+use Innmind\Html\{
+    Visitor\Elements,
+    Visitor\Head,
+    Exception\ElementNotFoundException
+};
+use Innmind\TimeContinuum\TimeContinuumInterface;
+use Innmind\Http\Message\{
+    RequestInterface,
+    ResponseInterface
+};
+use Innmind\Immutable\MapInterface;
+
+final class CharsetParser implements ParserInterface
+{
+    use HtmlTrait;
+
+    private $reader;
+    private $clock;
+
+    public function __construct(
+        ReaderInterface $reader,
+        TimeContinuumInterface $clock
+    ) {
+        $this->reader = $reader;
+        $this->clock = $clock;
+    }
+
+    public function parse(
+        RequestInterface $request,
+        ResponseInterface $response,
+        MapInterface $attributes
+    ): MapInterface {
+        $start = $this->clock->now();
+
+        if (!$this->isHtml($attributes)) {
+            return $attributes;
+        }
+
+        $document = $this->reader->read($response->body());
+
+        try {
+            $metas = (new Elements('meta'))(
+                (new Head)($document)
+            );
+        } catch (ElementNotFoundException $e) {
+            return $attributes;
+        }
+
+        $meta = $metas
+            ->filter(function(NodeInterface $node): bool {
+                return $node instanceof ElementInterface;
+            })
+            ->filter(function(ElementInterface $meta): bool {
+                return $meta->attributes()->contains('charset');
+            });
+
+        if ($meta->size() !== 1) {
+            return $attributes;
+        }
+
+        return $attributes->put(
+            self::key(),
+            new Attribute(
+                self::key(),
+                $meta
+                    ->current()
+                    ->attributes()
+                    ->get('charset')
+                    ->value(),
+                $this
+                    ->clock
+                    ->now()
+                    ->elapsedSince($start)
+                    ->milliseconds()
+            )
+        );
+    }
+
+    public static function key(): string
+    {
+        return 'charset';
+    }
+}
