782f0206fe1795cfd9f456c29a3a8e5b307fb8b3
Baptiste Langlade <baptouuuu@gmail.com>
2016-11-05 13:57:30 +0100

    add image specification

---
+++ tests/Specification/ImageTest.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Crawler\Specification;
+
+use Innmind\Crawler\Specification\Image;
+use Innmind\Filesystem\MediaType\MediaType;
+
+class ImageTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider cases
+     */
+    public function testIsSatisfiedBy(bool $expected, string $type)
+    {
+        $spec = new Image;
+
+        $this->assertSame(
+            $expected,
+            $spec->isSatisfiedBy(MediaType::fromString($type))
+        );
+    }
+
+    public function cases(): array
+    {
+        return [
+            [true, 'image/jpg'],
+            [true, 'image/jpeg'],
+            [true, 'image/png'],
+            [true, 'image/png+whatever'],
+            [true, 'image/vnd.webp+whatever'],
+            [false, 'application/pdf'],
+        ];
+    }
+}
