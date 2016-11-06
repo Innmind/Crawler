6b0aa4987f7a421b18ff80f00ce143ef233bdbef
Baptiste Langlade <baptouuuu@gmail.com>
2016-11-06 17:34:21 +0100

    add open graph image parser

---
+++ Tests/Parser/Html/OpenGraph/ImageParserTest.php
@@ -0,0 +1,214 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Crawler\Parser\Html\OpenGraph;
+
+use Innmind\Crawler\{
+    Parser\Html\OpenGraph\ImageParser,
+    HttpResource\AttributeInterface,
+    HttpResource\Attribute,
+    ParserInterface,
+    Parser\Http\ContentTypeParser
+};
+use Innmind\TimeContinuum\{
+    TimeContinuumInterface,
+    PointInTimeInterface,
+    ElapsedPeriod
+};
+use Innmind\Http\{
+    Message\RequestInterface,
+    Message\ResponseInterface
+};
+use Innmind\Filesystem\{
+    Stream\StringStream,
+    MediaType\MediaType
+};
+use Innmind\Html\{
+    Reader\Reader,
+    Translator\NodeTranslators as HtmlTranslators
+};
+use Innmind\Xml\Translator\{
+    NodeTranslator,
+    NodeTranslators
+};
+use Innmind\Url\UrlInterface;
+use Innmind\Immutable\{
+    Map,
+    MapInterface,
+    SetInterface
+};
+
+class ImageParserTest extends \PHPUnit_Framework_TestCase
+{
+    private $parser;
+    private $clock;
+
+    public function setUp()
+    {
+        $this->parser = new ImageParser(
+            new Reader(
+                new NodeTranslator(
+                    NodeTranslators::defaults()->merge(
+                        HtmlTranslators::defaults()
+                    )
+                )
+            ),
+            $this->clock = $this->createMock(TimeContinuumInterface::class)
+        );
+    }
+
+    public function testInterface()
+    {
+        $this->assertInstanceOf(ParserInterface::class, $this->parser);
+    }
+
+    public function testKey()
+    {
+        $this->assertSame('preview', ImageParser::key());
+    }
+
+    public function testDoesntParseWhenNoContentType()
+    {
+        $request = $this->createMock(RequestInterface::class);
+        $response = $this->createMock(ResponseInterface::class);
+        $expected = new Map('string', AttributeInterface::class);
+
+        $attributes = $this->parser->parse(
+            $request,
+            $response,
+            $expected
+        );
+
+        $this->assertSame($expected, $attributes);
+    }
+
+    public function testDoesntParseWhenNotHtml()
+    {
+        $request = $this->createMock(RequestInterface::class);
+        $response = $this->createMock(ResponseInterface::class);
+        $expected = (new Map('string', AttributeInterface::class))
+            ->put(
+                ContentTypeParser::key(),
+                new Attribute(
+                    ContentTypeParser::key(),
+                    MediaType::fromString('text/csv'),
+                    0
+                )
+            );
+
+        $attributes = $this->parser->parse(
+            $request,
+            $response,
+            $expected
+        );
+
+        $this->assertSame($expected, $attributes);
+    }
+
+    public function testDoesntParseWhenNoHead()
+    {
+        $response = $this->createMock(ResponseInterface::class);
+        $response
+            ->expects($this->once())
+            ->method('body')
+            ->willReturn(new StringStream('<html></html>'));
+        $expected = (new Map('string', AttributeInterface::class))
+            ->put(
+                ContentTypeParser::key(),
+                new Attribute(
+                    ContentTypeParser::key(),
+                    MediaType::fromString('text/html'),
+                    0
+                )
+            );
+
+        $attributes = $this->parser->parse(
+            $this->createMock(RequestInterface::class),
+            $response,
+            $expected
+        );
+
+        $this->assertSame($expected, $attributes);
+    }
+
+    public function testParse()
+    {
+        $response = $this->createMock(ResponseInterface::class);
+        $response
+            ->expects($this->once())
+            ->method('body')
+            ->willReturn(new StringStream(<<<HTML
+<html>
+<head>
+    <meta property="og:title" content="The Rock" />
+    <meta property="og:type" content="video.movie" />
+    <meta property="og:url" content="http://www.imdb.com/title/tt0117500/" />
+    <meta property="og:image" content="http://ia.media-imdb.com/images/rock.jpg" />
+    <meta property="og:image" content="http://ia.media-imdb.com/images/rock2.jpg" />
+</head>
+</html>
+HTML
+            ));
+        $notExpected = (new Map('string', AttributeInterface::class))
+            ->put(
+                ContentTypeParser::key(),
+                new Attribute(
+                    ContentTypeParser::key(),
+                    MediaType::fromString('text/html'),
+                    0
+                )
+            );
+        $this
+            ->clock
+            ->expects($this->exactly(2))
+            ->method('now')
+            ->will(
+                $this->onConsecutiveCalls(
+                    $start = $this->createMock(PointInTimeInterface::class),
+                    $end = $this->createMock(PointInTimeInterface::class)
+                )
+            );
+        $end
+            ->expects($this->once())
+            ->method('elapsedSince')
+            ->with($start)
+            ->willReturn(new ElapsedPeriod(42));
+
+        $attributes = $this->parser->parse(
+            $this->createMock(RequestInterface::class),
+            $response,
+            $notExpected
+        );
+
+        $this->assertNotSame($notExpected, $attributes);
+        $this->assertInstanceOf(MapInterface::class, $attributes);
+        $this->assertSame('string', (string) $attributes->keyType());
+        $this->assertSame(
+            AttributeInterface::class,
+            (string) $attributes->valueType()
+        );
+        $this->assertCount(2, $attributes);
+        $this->assertTrue($attributes->contains('preview'));
+        $preview = $attributes->get('preview');
+        $this->assertSame('preview', $preview->name());
+        $this->assertInstanceOf(
+            SetInterface::class,
+            $preview->content()
+        );
+        $this->assertSame(
+            UrlInterface::class,
+            (string) $preview->content()->type()
+        );
+        $this->assertCount(2, $preview->content());
+        $this->assertSame(
+            'http://ia.media-imdb.com/images/rock.jpg',
+            (string) $preview->content()->current()
+        );
+        $preview->content()->next();
+        $this->assertSame(
+            'http://ia.media-imdb.com/images/rock2.jpg',
+            (string) $preview->content()->current()
+        );
+        $this->assertSame(42, $preview->parsingTime());
+    }
+}
